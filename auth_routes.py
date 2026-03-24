2589ff36844addb114506e6b0ae1841101ceea31
Egurt-0 <eduardogurovsky@gmail.com>
2026-03-23 21:27:33 -0300

    login_almost_finished

--- auth_routes.py
+++ auth_routes.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends
 from dependencies import pegar_dependecias
 from models import Usuario
+from main import bcrypts_context
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 @auth_router.get("/")
 async def autenticar():
@@ -13,7 +14,8 @@ async def login(email: str, senha: str, nome:str, session = Depends(pegar_depend
     if usuario:
         return{"mensage": "email already exists"}
     else:
-        novo_usuario = Usuario(nome, email, senha)
+        senha_criptografada = bcrypts_context.hash(senha[:72]) # transformando a senha em hash
+        novo_usuario = Usuario(nome, email, senha_criptografada)
         session.add(novo_usuario)
         session.commit()
         return {"Mensagem": "Sucessful Login"}
--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-app = FastAPI()
-from auth_routes import auth_router
-from order_routes import order_routes
-app.include_router(auth_router)
-app.include_router(order_routes)
 from passlib.context import CryptContext
 from dotenv import load_dotenv
 import os
-from os import getenv
+
+bcrypts_context = CryptContext(
+    schemes=["bcrypt"],
+    deprecated="auto"
+)
 
 load_dotenv()
-SECRET_KEY = os.getenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+app = FastAPI()
+
+from auth_routes import auth_router
+from order_routes import order_routes
+
+app.include_router(auth_router)
+app.include_router(order_routes)
